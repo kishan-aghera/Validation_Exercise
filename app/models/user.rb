eaaac7cd0c22a10c45a7a888cef0db3af17fdc31
Kishan Aghera <55836816+kishan-aghera@users.noreply.github.com>
2021-02-11 13:27:35 +0530

    Updated user.rb file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  before_create :normalize_input
+  before_save :normalize_name, :normalize_remaining_input
+  before_validation :email_downcase
+  
   validates :first_name, :last_name, :city, :designation, :company, :linkedin_id, presence: true
   validates :email, presence: true, format: { with: /\A([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)\z/, message: "Invalid" }
   validates :phone, presence: true, format: { with: /\+?\d[\d -]{8,12}\d/, message: "Invalid" }
@@ -8,12 +10,18 @@ class User < ApplicationRecord
   validates :salary, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   
   private
-    def normalize_input
+    def normalize_name
       self.first_name = first_name.downcase.titleize
       self.last_name = last_name.downcase.titleize
+    end
+
+    def email_downcase
+      self.email = email.downcase
+    end
+
+    def normalize_remaining_input  
       self.city = city.downcase.titleize
       self.designation = designation.downcase.titleize
       self.company = company.downcase.titleize
-      self.email = email.downcase
     end
 end
